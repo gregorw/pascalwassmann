73921dc022fefa748355e1c41d06b8af001b29a8
Gregor Wassmann <gregor.wassmann@gmail.com>
2019-05-28 23:42:17 +0200

    Remove vue/nuxt branding

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,15 @@
 <template>
   <section class="container">
     <div>
-      <logo />
       <h1 class="title">
-        pascalwassmann
+        Pascal Wassmann
       </h1>
       <h2 class="subtitle">
         Architect
       </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
-          >Documentation</a
-        >
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-          >GitHub</a
-        >
-      </div>
     </div>
   </section>
 </template>
-
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
 
 <style>
 .container {
@@ -59,9 +37,5 @@ export default {
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
