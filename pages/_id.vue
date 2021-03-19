efb650ec1b7c243a64c27ba33f13deb77dd9b3a1
Gregor Wassmann <gregor.wassmann@gmail.com>
2021-03-19 20:44:46 +0100

    Preserve newlines

--- pages/_id.vue
+++ pages/_id.vue
@@ -2,12 +2,13 @@
   <div class="page">
     <Back />
     <h1>{{ title }}</h1>
-    <RichTextRenderer :document="body" />
+    <RichTextRenderer :document="body" :node-renderers="renderNodes()" />
   </div>
 </template>
 
 <script>
 import RichTextRenderer from 'contentful-rich-text-vue-renderer'
+import { BLOCKS } from '@contentful/rich-text-types'
 import createClient from '~/plugins/contentful'
 import Back from '~/components/Back'
 
@@ -27,6 +28,23 @@ export default {
       const page = response.items[0]
       return page.fields
     })
+  },
+  methods: {
+    renderNodes () {
+      return {
+        [BLOCKS.PARAGRAPH] (node, key, h, next) {
+          const preservedNewlines = next(node.content).map((text) => {
+            if (typeof text === 'string' || text instanceof String) {
+              const withNewlines = text.split('\n').map(part => [part, h('br')]).flat()
+              withNewlines.pop()
+              return withNewlines
+            }
+            return text
+          })
+          return h('p', preservedNewlines)
+        }
+      }
+    }
   }
 }
 </script>
