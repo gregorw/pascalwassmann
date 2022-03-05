e26946d90eccfff359bfafe3090ad18b85241b69
Gregor Wassmann <gregor.wassmann@gmail.com>
2022-03-05 21:58:24 +0100

    Add Options…

--- components/Options.vue
+++ components/Options.vue
@@ -1,29 +1,64 @@
 <template>
   <aside class="options">
-    Options
+    <b>Text Optionen</b>
+    <template v-for="option in options">
+      <label :key="option.class">
+        <input v-model="textOption" v-on:change="setText" :value="option.class" name="text" type="radio">
+        {{ option.name }}
+      </label>
+    </template>
   </aside>
 </template>
 
 <style lang="sass" scoped>
 .options
   position: absolute
-  right: 0
+  right: -19rem
   top: 0
   bottom: 0
-  width: 50px
+  opacity: 0
+  transition: all 0.3s ease-in-out
+  background: rgba(black, 0.85)
+  color: white
+  padding: 1rem
+  width: 20rem
 
   &:hover
-    width: 20rem
-    background: yellow
+    opacity: 1
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2)
+    right: 0
+
+  label
+    display: block
 </style>
 
 <script>
 export default {
-  name: 'Back',
-  props: {
-    text: {
-      type: String, // We expect an array of pages that we need for our navigation
-      default: '✕' // ╳ ←
+  data () {
+    return {
+      options: [{
+        name: 'Bisher',
+        class: 'text-none'
+      },
+      {
+        name: 'Hintergrund weiss',
+        class: 'text-bg-white'
+      },
+      {
+        name: 'Hintergrund transparent',
+        class: 'text-bg-transparent'
+      },
+      {
+        name: '«Schatten» weiss',
+        class: 'text-shadow-white'
+      }],
+      textOption: 'text-none'
+    }
+  },
+  methods: {
+    setText (event) {
+      document.body.classList.remove(...this.options.map(o => o.class))
+      document.body.classList.add(event.target.value)
     }
   }
 }
