7d93697a4415baed8a3bac7aa00d22b687d76b1b
Gregor Wassmann <gregor.wassmann@gmail.com>
2022-08-01 23:03:49 +0200

    Fix builds

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,8 +5,8 @@ require('dotenv').config()
 const contentful = createClient()
 
 export default {
-  mode: 'universal',
-
+  target: 'static',
+  ssr: false,
   env: {
     CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
     CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN
