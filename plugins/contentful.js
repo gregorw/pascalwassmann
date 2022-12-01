4777e2e79569c8d5c17d454d6a0b28bef13ac248
Gregor Wassmann <gregor.wassmann@gmail.com>
2022-12-01 09:21:05 +0100

    Fix preview

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -3,7 +3,8 @@ const contentful = require('contentful')
 function createClient () {
   return contentful.createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    host: process.env.CONTENTFUL_HOST
   })
 }
 
